fe67cc0065df84c2c92ebbb546f3ca4f0e5f2445
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-06-09 17:06:02 -0300

    add handlers and DotCounter

--- ProjOO/Cap6/Chain_of_Responsability/main.py
+++ ProjOO/Cap6/Chain_of_Responsability/main.py
@@ -1,14 +1,21 @@
 from components.Console import Console
 from components.CharCounter.SpaceCounter import SpaceCounter
 from components.CharCounter.ACounter import ACounter
+from components.CharCounter.DotCounter import DotCounter
+from components.CharCounterHandler import CharCounterHandler
 
 console = Console()
 console.setText()
 text = console.getText()
 
-space_counter = SpaceCounter()
-a_counter = ACounter()
+handler = CharCounterHandler()
+
+space_counter = SpaceCounter(handler)
+a_counter = ACounter(handler)
+dot_counter = DotCounter(handler)
 
 space_counter.setNext(a_counter)
-
+a_counter.setNext(dot_counter)
 space_counter.countChar(text)
+
+handler.printResults()
