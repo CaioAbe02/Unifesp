24618311c92c28794261f0bd650e49609ae485c6
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-06-09 23:14:43 -0300

    update exemple

--- ProjOO/Cap6/Mediator/main.py
+++ ProjOO/Cap6/Mediator/main.py
@@ -1,11 +1,19 @@
-from components.Aircraft import Aircraft
-from components.ControlTower import ControlTower
+from components.Aircraft.Aircraft import Aircraft
+from components.ControlTower.ControlTower import ControlTower
 
-torre:ControlTower = ControlTower()
+torre: ControlTower = ControlTower()
 
 boeing747: Aircraft = Aircraft("Boeing-747")
+boeing737: Aircraft = Aircraft("Boeing-737")
+embraer190e2: Aircraft = Aircraft("Embraer 190-E2")
+
 boeing747.setControlTower(torre)
+boeing737.setControlTower(torre)
+embraer190e2.setControlTower(torre)
 
 torre.addAircraft(boeing747)
+torre.addAircraft(boeing737)
+torre.addAircraft(embraer190e2)
 
 boeing747.requestLanding()
+boeing747.land()
